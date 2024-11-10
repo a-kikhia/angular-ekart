a17be07535687be6a624ab8d912dd25e99da663f
Abdullatif Kikhia <abdullatif.kikhia@wps.de>
2024-11-10 20:47:36 +0100

    some stuff

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'product-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css',
+})
+export class ProductListComponent {
+  // name = 'Befo Kik';
+  addToCart: number = 0;
+  product = {
+    name: 'Iphone 13',
+    price: 999,
+    color: 'Black',
+    discount: 8.5,
+    inStock: 10,
+    pImage: 'assets/images/iphone.jpeg',
+  };
+
+  getDiscountedPrice() {
+    return (
+      this.product.price - (this.product.price * this.product.discount) / 100
+    );
+  }
+
+  onNamechange(event: any) {
+    // this.name = event.target.value;
+  }
+
+  incerementValueInCart(){
+    if(this.addToCart < this.product.inStock)
+      this.addToCart++;
+  }
+  
+  decrementValueInCart(){
+   if(this.addToCart > 0 )
+     this.addToCart--;
+  }
+
+
+}
