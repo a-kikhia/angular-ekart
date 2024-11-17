566d03c90fdbe18be118eca6740dc8e943899c21
Abdullatif Kikhia <abdullatif.kikhia@wps.de>
2024-11-17 19:27:21 +0100

    Search bar added

---
+++ src/app/product-list/search/search.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css'
+})
+export class SearchComponent {
+  searchText: string = 'Mens Wear'
+
+
+  // updateSearchText(event: any) {
+  //   this.searchText = event.target.value
+  // }
+}
