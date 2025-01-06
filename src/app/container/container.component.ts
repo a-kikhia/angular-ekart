75daef6bc9c75a36372f36b11fcf235fc2d45f3a
Abdullatif Kikhia <abdullatif.kikhia@wps.de>
2025-01-06 17:18:22 +0100

    ngContent

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -3,11 +3,12 @@ import { SearchComponent } from './search/search.component';
 import { NgForOf, NgIf } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { FeaturedBrandsComponent } from './featured-brands/featured-brands.component';
 
 @Component({
   selector: 'app-container',
   standalone: true,
-  imports: [SearchComponent, NgForOf, ProductListComponent, ProductDetailComponent, NgIf],
+  imports: [SearchComponent, NgForOf, ProductListComponent, ProductDetailComponent, NgIf, FeaturedBrandsComponent],
   templateUrl: './container.component.html',
   styleUrl: './container.component.css',
 })
