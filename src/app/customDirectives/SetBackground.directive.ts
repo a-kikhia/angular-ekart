5e1ed5db0bb3a7a7d1ade61f0021577c811e5b78
Abdullatif Kikhia <abdullatif.kikhia@wps.de>
2025-01-12 13:05:53 +0100

    renderer2

--- src/app/customDirectives/SetBackground.directive.ts
+++ src/app/customDirectives/SetBackground.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   standalone: true,
@@ -8,12 +8,16 @@ export class SetBackground implements OnInit{
 
   // private element: ElementRef;
 
-  constructor(private element: ElementRef) {
+  constructor(private element: ElementRef, private renderer: Renderer2) {
     // this.element = element;
+
   }
 
   ngOnInit(): void {
-    this.element.nativeElement.style.backgroundColor = '#36454F';
-    this.element.nativeElement.style.color = 'white';
+    // this.element.nativeElement.style.backgroundColor = '#36454F';
+    // this.element.nativeElement.style.color = 'white';
+    this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', '#36454F');
+    this.renderer.setStyle(this.element.nativeElement, 'color', 'white');
+    // this.renderer.setAttribute(this.element.nativeElement, 'title', 'This is example Title');
   }
 }
